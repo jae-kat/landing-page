e10106edfc71aca3fee9616d88fb9abda524768b
katharina jäger <jaeger.kt@gmail.com>
2022-01-24 16:51:45 +0100

    Add global styles, add components to app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import './App.css';
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import Header from './Header';
+import Nav from './Nav';
+
+const globalStyles = css`
+  box-sizing: border-box;
+  font-family: sans-serif;
+`;
 
 function App() {
-  return <div>HelloWorld</div>;
+  return (
+    <div css={globalStyles}>
+      <Nav />
+      <Header />
+    </div>
+  );
 }
 
 export default App;
