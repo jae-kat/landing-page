3754a656ea8a7173a8607435311bb7a54eb2cdfa
katharina jäger <jaeger.kt@gmail.com>
2022-01-24 21:55:01 +0100

    Add google fonts

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import './App.css';
 /** @jsxImportSource @emotion/react */
+import './App.css';
 import { css } from '@emotion/react';
 import Header from './Header';
 import Nav from './Nav';
 
 const globalStyles = css`
   box-sizing: border-box;
-  font-family: sans-serif;
+  font-family: Inter, sans-serif;
 `;
 
 function App() {
