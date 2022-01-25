5f30da8f8c81e244038965b99e4b0fa9a953de2d
katharina jäger <jaeger.kt@gmail.com>
2022-01-25 17:27:02 +0100

    Add svg icons to images folder

---
+++ src/images/GreenSquare.js
@@ -0,0 +1,22 @@
+export default function GreenSquare() {
+  return (
+    <svg viewBox="0 0 24 24" focusable="false" width="4rem" color="#3ebf8f">
+      <g>
+        <path
+          fill="currentColor"
+          d="M22 6.5L12 12 2 6.49a2.35 2.35 0 01.85-.822l7.935-4.364a2.581 2.581 0 012.43 0l7.934 4.364c.353.2.646.493.851.832z"
+          opacity=".4"
+        />
+        <path
+          fill="currentColor"
+          d="M12 12v11c-.37 0-.737-.086-1.07-.25l-7.754-4.542A2.394 2.394 0 012 16.145v-8.94C2.002 6.781 2 7 2 6.5L12 12z"
+          opacity=".7"
+        />
+        <path
+          fill="currentColor"
+          d="M22 7.204v8.941a2.394 2.394 0 01-1.177 2.063l-7.788 4.554A2.437 2.437 0 0112 23V12l10-5.5c0 .5-.002.282 0 .704z"
+        />
+      </g>
+    </svg>
+  );
+}
